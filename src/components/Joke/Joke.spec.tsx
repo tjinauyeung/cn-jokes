f930596ba401d6f02bbaf395ead0a8380818a63c
Tjin Au Yeung <tjinauyeung@gmail.com>
2019-04-17 14:48:21 +0200

    tests: configure jest

---
+++ src/components/Joke/Joke.spec.tsx
@@ -0,0 +1,28 @@
+// import * as React from "react";
+// import { Joke } from "./index";
+// import * as TestRenderer from "react-test-renderer";
+
+// // const props = {
+// //   joke: {
+// //     id: "123",
+// //     joke: "Foo foo foo"
+// //   },
+// //   onClick: () => "foo",
+// //   buttonText: "foo"
+// // };
+// // const component = TestRenderer.create(
+// //   <Joke
+// //     joke={props.joke}
+// //     buttonText={props.buttonText}
+// //     onClick={props.onClick}
+// //   />
+// // );
+// // const instance = component.getInstance();
+
+// describe("Joke", () => {
+//   it("render a div", () => {});
+
+//   it("foo", () => {
+//     expect(1 + 2).toBe(3);
+//   });
+// });
