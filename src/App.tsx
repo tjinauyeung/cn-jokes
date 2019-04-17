a0a4e3dd751b8359c64f0127533698123212a088
Tjin Au Yeung <tjinauyeung@gmail.com>
2019-04-17 10:25:31 +0200

    setup: skeleton app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
 import * as React from "react";
+import { useState } from "react";
 
-export const App = () => <div>App</div>;
+const TAB_JOKES = "tab_jokes";
+const TAB_FAVOURITES = "tab_favourites";
+
+export const App = () => {
+  const [activeTab, setActiveTab] = useState(TAB_JOKES);
+
+  const toTab = tab => e => {
+    setActiveTab(tab);
+  };
+
+  const isActive = tab => {
+    return activeTab === tab;
+  };
+
+  return (
+    <div className="wrapper">
+      <h1>Chuck Norris</h1>
+      <button onClick={toTab(TAB_JOKES)}>Jokes</button>
+      <button onClick={toTab(TAB_FAVOURITES)}>Fav. jokes</button>
+      {isActive(TAB_JOKES) && <div>List of jokes</div>}
+      {isActive(TAB_FAVOURITES) && <div>List of favourite jokes</div>}
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./App";
+import "./index.scss";
 
 const root = document.querySelector("#root");
 ReactDOM.render(<App />, root);
